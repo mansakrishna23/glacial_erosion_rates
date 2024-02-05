93092dc548981389c1a4aad9de4f05b5553a6541
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-02-04 21:04:16 -0500

    Update Be plot

--- src/beryllium.jl
+++ src/beryllium.jl
@@ -6,7 +6,8 @@ h = plot(framestyle=:box,
     xlabel="pH",
     ylabel="Be [mol/L]",
     yscale=:log10,
-    ylims=10.0.^(-12, -2),
+    ylims=10.0.^(-12, -4),
+    yticks=10.0.^(-12:-4),
     xlims=(4,9),
 )
 
@@ -22,28 +23,39 @@ Beₐ = Ksp_BeOH2./OH.^2
 # BeOH2 saturation
 plot!(pH, Beₐ, color=:black, lw=2, label="")
 plot!(pH, Beₐ, color=:black, lw=2, fillto=1.0, alpha=0.15, label="")
-annotate!(pH[200:200],Beₐ[200:200], text("BeOH₂ saturation", 10, :left, :bottom, color=:black, rotation=-33.33333))
+annotate!(pH[200:200],Beₐ[200:200], text("BeOH₂ saturation", 10, :left, :bottom, color=:black, rotation=-40))
+
+# Rainwater pH
+vline!([4.77,], color=:red, linestyle=:dash, label="")
+annotate!([4.77,],[max(yl...),],text("Median rainwater ", 10, :right, :bottom, color=:red, rotation=90))
+
+# Soil pH
+vline!([5.72,], color=:darkorange, linestyle=:dash, label="")
+annotate!([5.72,],[max(yl...),], text("Median soil ", 10, :right, :bottom, color=:darkorange, rotation=90))
 
 # Ocean Be
 hline!([2.33E-02*1e-9,], ribbon=(10e-12, 40e-12), fillalpha=0.15, color=:blue, label="")
 annotate!([4.1,],[2.33E-02*1e-9,],text("Seawater Be", 10, :left, :bottom, color=:darkblue, ))
 # Ocean pH
 yl = ylims()
-vline!([8.2,], color=:blue, linestyle=:dot, label="")
-plot!([7.8, 8.4], fill(max(yl...), 2), fillto=min(yl...), color=:blue, alpha=0.15, label="")
-annotate!([8.2,],[sqrt(prod(yl)),],text("Seawater pH", 10, :center, :bottom, color=:darkblue, rotation=90))
-
+vline!([8.2,], color=:blue, label="")
+plot!([8.2-0.14, 8.2+0.14], fill(max(yl...), 2), fillto=min(yl...), color=:blue, alpha=0.15, label="")
+annotate!([8.2,],[max(yl...),],text("Seawater pH ", 10, :right, :bottom, color=:darkblue, rotation=90))
 
 ## --- manually compiled Be
 
 water = importdataset("data/beryllium.tsv", '\t', importas=:Tuple)
-plot!(water.pH, water.Be_dissolved_nmol_kg.*1e-9, color=:darkblue, seriestype=:scatter, label="Observed surface water Beaq")
+plot!(water.pH, water.Be_dissolved_nmol_kg.*1e-9, 
+    color=:darkblue, 
+    seriestype=:scatter, 
+    label="Terrestrial and estuarine waters",
+    legend=:bottomleft,
+)
 # plot!(water.pH, water.Be_nmol_kg.*1e-9, seriestype=:scatter, label="")
 # plot!(water.pH, water.Be_colloidal_nmol_kg.*1e-9, seriestype=:scatter, label="")
 
 ## ---
 savefig("beryllium.pdf")
-
 
 
 ## --- Net denudation / sediment production
@@ -67,3 +79,4 @@ beprod/besed
 besedreq = beprod/1e-7
 besedreq/besed
 
+## ---
