709b8dd97edb2abf1feab35c9dcfe11e4fedbef4
C. Brenhin Keller <cbkeller@dartmouth.edu>
2024-03-15 11:48:37 -0400

    Add Eocene seawater pH to Be figure

--- src/beryllium.jl
+++ src/beryllium.jl
@@ -25,23 +25,29 @@ Beₐ = Ksp_BeOH2./OH.^2
 plot!(pH, Beₐ, color=:black, lw=2, label="")
 plot!(pH, Beₐ, color=:black, lw=2, fillto=1.0, alpha=0.15, label="")
 annotate!(pH[200:200],Beₐ[200:200], text("BeOH₂ saturation", 10, :left, :bottom, color=:black, rotation=-40, family=:Helvetica))
+yl = ylims()
 
 # Rainwater pH
 vline!([4.77,], color=:red, linestyle=:dash, label="")
-annotate!([4.77,],[max(yl...),],text("Median rainwater ", 10, :right, :bottom, color=:red, rotation=90, family=:Helvetica))
+annotate!([4.77,],[max(yl...),],text("Median rainwater pH ", 10, :right, :bottom, color=:red, rotation=90, family=:Helvetica))
 
 # Soil pH
 vline!([5.72,], color=:darkorange, linestyle=:dash, label="")
-annotate!([5.72,],[max(yl...),], text("Median soil ", 10, :right, :bottom, color=:darkorange, rotation=90, family=:Helvetica))
+annotate!([5.72,],[max(yl...),], text("Median soil pH ", 10, :right, :bottom, color=:darkorange, rotation=90, family=:Helvetica))
+
+
+# Eocene seqwater pH (via Boudreau 2019 fig. 8 data)
+vline!([7.77,], color=:cyan, linestyle=:dash, label="")
+plot!([7.77-0.15, 7.77+0.15], fill(max(yl...), 2), fillto=min(yl...), color=:cyan, alpha=0.15, label="")
+annotate!([7.77,],[max(yl...),], text("Eocene seawater pH ", 10, :right, :bottom, color=:darkcyan, rotation=90, family=:Helvetica))
 
 # Ocean Be
 hline!([2.33E-02*1e-9,], ribbon=(10e-12, 40e-12), fillalpha=0.15, color=:blue, label="")
-annotate!([4.1,],[2.33E-02*1e-9,],text("Seawater Be", 10, :left, :bottom, color=:darkblue, family=:Helvetica))
+annotate!([4.1,],[2.33E-02*1e-9,],text("Modern Seawater Be", 10, :left, :bottom, color=:darkblue, family=:Helvetica))
 # Ocean pH
-yl = ylims()
 vline!([8.2,], color=:blue, label="")
 plot!([8.2-0.14, 8.2+0.14], fill(max(yl...), 2), fillto=min(yl...), color=:blue, alpha=0.15, label="")
-annotate!([8.2,],[max(yl...),],text("Seawater pH ", 10, :right, :bottom, color=:darkblue, rotation=90, family=:Helvetica))
+annotate!([8.2,],[max(yl...),],text("Modern Seawater pH ", 10, :right, :bottom, color=:darkblue, rotation=90, family=:Helvetica))
 
 ## --- manually compiled Be
 
